97b3ee9f39776a38de1787ca2e6ce101003a2e0f
Yeraly-M <40256@iitu.edu.kz>
2025-04-02 10:52:35 +0500

    2nd week

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,13 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('booking/', views.booking, name='booking'),
+    path('book/<int:table_id>/', views.book_table, name='book_table'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,50 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Table
 
 def home(request):
-    return render(request, 'home.html')
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('booking')
+    return render(request, 'home.html', {'form': form})
+
+def booking(request):
+    tables = Table.objects.all()
+    return render(request, 'booking.html', {'tables': tables})
+
+def book_table(request, table_id):
+    table = Table.objects.get(id=table_id)
+    if not table.is_booked:
+        table.is_booked = True
+        table.save()
+    return redirect('booking')
+
+def login_view(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('booking')
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('booking')
+    return render(request, 'register.html', {'form': form})
